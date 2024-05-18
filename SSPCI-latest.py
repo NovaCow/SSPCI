180661db5ad38a88ab1a23ae7d6d350a2a15b585
Nova <155397217+NovaCow@users.noreply.github.com>
2024-05-18 23:05:34 +0200

    Add files via upload

--- SSPCI-latest.py
+++ SSPCI-latest.py
@@ -2,13 +2,22 @@ import cmd
 import os
 import platform
 import shutil
+import importlib
+from typing import List
 
-# Currently on Version 0.5.1-alpha
+
+# Currently on Version 0.6.0-unstable
 # This is the SSPCI, Super Simple Python Command Interpreter, made in... well... Python
 # Should be supported on (almost) everything. As it uses common libraries.
 # I should at some point add error handling. Because I don't have that currently.
 # But I'm working on error handling now! :)
 # I also need to put more comments everywhere, I barely use them.
+# I have a todo list that I want to complete before version 1.0.0:
+# -Add scripting (Goal is before 0.8.0)
+# -Add FTP support (Goal is before 0.7.0)
+# -Add some kind of ping or http command [IN PROGRESS] (Goal is to finish before v0.5.2)
+# -Somehow make a package manager. [IN PROGRESS] (Goal is to finish before v0.6.0)
+# -Add support for running older versions within the shell. (Goal is before v0.7.0)
 # Now I'm mimicking the Linux prompt. Or at least the one that comes with bash.
 # Lots of pre-shell config.
 
@@ -16,24 +25,26 @@ import shutil
 class SSPCI(cmd.Cmd):
     global envi
     global enviVar
+    global pkg
     splitWD = os.getcwd()
     cwd = splitWD.rsplit("/", 1)
     promptcwd = cwd[1]
+    pkg = []
     intro = "This is the SSPCI Shell, type help or ? to list current supported commands.\n"
     prompt = "[sspci@" + platform.system() + " " + promptcwd + "]$ "
     file = None
-    print("SSPCI version 0.5.0-alpha running on " + platform.system() + " " + platform.release())
+    print("SSPCI version 0.6.0-unstable running on " + platform.system() + " " + platform.release())
 
     # The structure for the 'envi' variable is: OS/PATHTYPE. Examples: Linux/POSIX, Windows/WinNT
+    # It mostly defines how SSPCI should handle paths and commands.
     # You can use a different OS, such as Haiku or FreeBSD, the structure would be:
     # Haiku/POSIX, FreeBSD/POSIX
+    # Since I don't have either of those, I don't know if FreeBSD gets recognized as Linux.
+    # I want to add native support for FreeBSD and other BSD-based systems. So I might discard Linux and switch to Unix-like.
     if platform.system() == "Linux":
         if os.access("/root", os.R_OK):  # Probably the most unusable, unnecessary way to check if we're running as root.
             print("WARNING!! You might be running SSPCI as root! THIS CAN CAUSE IRREPARABLE DAMAGE!! USE AT YOUR OWN RISK!")
-            envi = "Linux/POSIX"
-        else:
-            envi = "Linux/POSIX"
-            pass
+        envi = "Linux/POSIX"
     elif platform.system() == "Darwin":
         print("You are running an unsupported operating system (Probably macOS). This should cause no harm.")
         envi = "Darwin/POSIX"
@@ -43,7 +54,7 @@ class SSPCI(cmd.Cmd):
     else:
         envi = "Unknown/UNKNOWN"
         print("SSPCI couldn't determine what type of system you are using.")
-        enviCheck = input("Please enter pathtype: (WinNT (Windows NT), POSIX (Linux, UNIX), POSIX-MAC (macOS) ")
+        enviCheck = input("Please enter pathtype: (WinNT (Windows NT), POSIX (Linux, UNIX), POSIX-MAC (macOS), POSIX-BSD (BSD-Based systems)) ")
         if enviCheck == "POSIX":
             envi = "Linux/POSIX"
         elif enviCheck == "POSIX-MAC":
@@ -53,7 +64,7 @@ class SSPCI(cmd.Cmd):
         else:
             print("SSPCI couldn't determine what type of system you are using.")
             print("Fatal error! E00, SSPCI failed to determine type of system!")
-            ErrIgnore = input("If you're ABSOLUTELY sure you can run SSPCI, type continue to continue. ")
+            ErrIgnore = input("If you're ABSOLUTELY sure you can run SSPCI, type continue to continue or press enter to exit. ")
             if ErrIgnore == "continue":
                 envi = input("Input environment like such: Linux/POSIX (OS/PATHTYPE): ")
             else:
@@ -68,7 +79,7 @@ class SSPCI(cmd.Cmd):
         A command that echoes what you input.
         """
         if echo:
-            print(echo)  # Who would in their right mind use this?!?
+            print(echo)  # Who would in their right mind use this?!?  # Once scripting comes it's pretty handy.
         else:
             print(" ")
 
@@ -91,6 +102,140 @@ class SSPCI(cmd.Cmd):
             print(str(FilesInDir))  # But the same issue persists as with a directory.
             # But it works, and that's what matters.
 
+    def do_synk(self, package):
+        """A package manager, somehow. Usage: synk -I <package>. For help: synk -? or --help"""
+        #pkg = []  # Only here to test, won't be here in main release of 0.6.0
+        global pkg
+        listOption = package.split(" ", -1)
+        try:
+            pkg
+        except UnboundLocalError:
+            print("Fatal error! synk can't access variable for storing package! Aborting any action!")
+            print("Exact error: SSPCI.module.synk encountered an UnboundLocalError for variable pkg.")
+            return False
+        except NameError:
+            print("Fatal error! synk can't access variable for storing package! Aborting any action!")
+            print("Exact error: SSPCI.module.synk encountered a NameError for variable pkg.")
+            return False
+        except:
+            print("Fatal error! An unknown error occurred accessing variable for storing packages! Aborting any action!")
+            print("No exact error information found.")
+            return False
+        if listOption[0] == "-I":
+            if 2 > len(listOption):
+                print("synk version 0.0.2")
+                print("No package specified. Try synk --help for help")
+            elif 2 < len(listOption):
+                print("synk version 0.0.2")
+                print("Too many targets specified. Try synk --help for help.")
+            else:
+                print("synk version 0.0.2")
+                print("As of now synk can only import Python libraries.")
+                if len("a") == 3:
+                    #print("Cannot install more than 3 packages. Cannot continue.")
+                    pass
+                else:
+                    #pkg.append(importlib.import_module(listOption[1]))  # Meant for testing.
+                    try:
+                        print("Installing package " + listOption[1] + "...")
+                        pkg.append(importlib.import_module(listOption[1]))
+                        print("Installation successful!")
+                    except ModuleNotFoundError:
+                        print("Installation failed. Package " + str(listOption[1]) + " doesn't exist.")
+                    except UnboundLocalError:
+                        print("Installation failed. Can't save package to non-existent location.")
+                        print("Warning! This error may mean Python can't access the variable used to store packages!")
+                        print("Please file a bug report if this error continues to happen, even after a reboot.")
+                    except:
+                        print("An unknown error occurred. Please try again.")
+        elif listOption[0] == "-L":
+            print("You have installed " + str(len(pkg)) + " package(s) via synk")
+        elif listOption[0] == "-R":
+            print("WARNING! This will remove ALL packages!")
+            confirm = input("Do you wish to continue? [y/n] ")
+            while confirm != "y" and confirm != "Y" and confirm != "n" and confirm != "N":
+                print("Invalid answer. Try again.")
+                confirm = input("Do you wish to continue? [y/n] ")
+            if confirm == "y" or confirm == "Y":
+                pkg = []
+            else:
+                print("Aborted.")
+
+
+    def do_synklist(self, none):
+        """Prints the amount of packages installed via synk. Usage: synklist"""
+        global pkg
+        print("You have installed " + str(len(pkg)) + " package(s) via synk")
+
+    def do_test(self, module):  # Was a proof-of-concept. Will be removed before release.
+        """Allows you to test an installed module via synk. Usage: test random"""
+        global pkg
+        if module == "random":
+            pkgList = input("Where is package 'random' stored? ")
+            pkgList = int(pkgList)
+            if pkgList == 2:
+                try:
+                    a = pkg[2].randint(0,10)
+                    print(a)
+                    print("Module random tested successfully")
+                except IndexError:
+                    print("No package in place 2! Please install a package with synk!")
+                except:
+                    print("Cannot verify module integrity, please install with synk -I random")
+            elif pkgList == 1:
+                try:
+                    a = pkg[1].randint(0,10)
+                    print(a)
+                    print("Module random tested successfully")
+                except IndexError:
+                    print("No package in place 1! Please install a package with synk!")
+                except:
+                    print("Cannot verify module integrity, please install with synk -I random")
+            elif pkgList == 0:
+                try:
+                    a = pkg[0].randint(0,10)
+                    print(a)
+                    print("Module random tested successfully")
+                except IndexError:
+                    print("No package in place 0! Please install a package with synk!")
+                except:
+                    print("Cannot verify module integrity, please install with synk -I random")
+            else:
+                pass
+        elif module == "math":
+            pkgList = input("Where is package 'math' stored? ")
+            pkgList = int(pkgList)
+            if pkgList == 2:
+                try:
+                    a = pkg[2].sqrt(10)
+                    print(a)
+                    print("Module math tested successfully")
+                except IndexError:
+                    print("No package in place 2! Please install a package with synk!")
+                except:
+                    print("Cannot verify module integrity, please install with synk -I random")
+            elif pkgList == 1:
+                try:
+                    a = pkg[1].sqrt(10)
+                    print(a)
+                    print("Module math tested successfully")
+                except IndexError:
+                    print("No package in place 1! Please install a package with synk!")
+                except:
+                    print("Cannot verify module integrity, please install with synk -I random")
+            elif pkgList == 0:
+                try:
+                    a = pkg[0].sqrt(10)
+                    print(a)
+                    print("Module math tested successfully")
+                except IndexError:
+                    print("No package in place 0! Please install a package with synk!")
+                except:
+                    print("Cannot verify module integrity, please install with synk -I random")
+            else:
+                pass
+        else:
+            pass
     def do_dir(self, listedDir):  # The same as ls, but for Windows users.
         """Lists current files in the entered directory.
         On Windows: dir d:/example/example
@@ -122,12 +267,11 @@ class SSPCI(cmd.Cmd):
                     splitWD = os.getcwd()
                     if os.getcwd() == "/":  # An easy way to avoid checking indexes
                         SSPCI.prompt = "[sspci@" + platform.system() + " " + os.getcwd() + "]$ "
-                    elif os.getcwd() == "C:\\":  # Although I haven't really thought of Windows users.
+                    elif os.getcwd() == "C:\\" or os.getcwd() == "\\":  # Although I haven't really thought of Windows users.
                         SSPCI.prompt = "[sspci@" + platform.system() + " " + os.getcwd() + "]$ "
                     else:
                         cwd = splitWD.rsplit("/", 1)
-                        promptcwd = cwd[1]  # This line is unnecessary, it'll work with cwd[1] anyway.
-                        SSPCI.prompt = "[sspci@" + platform.system() + " " + promptcwd + "]$ "
+                        SSPCI.prompt = "[sspci@" + platform.system() + " " + cwd[1] + "]$ "
                 else:
                     print("Cannot enter directory " + directory + ". Make sure you have permissions to access this directory.")
             else:
@@ -213,8 +357,8 @@ class SSPCI(cmd.Cmd):
 
     def do_bash(self, cmd):  # For some reason Python defaults to sh and not bash. Annoying.
         """Tries to start bash if installed. Usage: bash <cmd>. If you want to start a bash terminal, type bash start."""
-        if enviVar[0] != "Linux":
-            print("You don't seem to be running Linux, bash might therefore not be installed.")
+        if enviVar[0] != "Linux" and enviVar[0] != "Darwin":  # Does macOS have bash?
+            print("You don't seem to be running Linux or macOS, bash might therefore not be installed.")
             print("Falling back to standard python command interpreter.")
             if cmd:
                 os.system(str(cmd))  # str() is for if any goofball decides to pass an int.
@@ -248,9 +392,10 @@ class SSPCI(cmd.Cmd):
             processor = "Unable to get processor information."
         else:
             processor = platform.processor()
-        print("You are running SSPCI version 0.5.1 on Python " + platform.python_version())
+        print("You are running SSPCI version 0.6.0 on Python " + platform.python_version())
         print("OS: " + platform.system() + " " + platform.release())
         print("Pathtype: " + str(enviVar[1]))
+        print("SEV: " + str(envi))
         print(f"Architechure: {uname.machine}")
         print("Processor: " + processor)
         print("Working directory: " + os.getcwd())
@@ -261,6 +406,10 @@ class SSPCI(cmd.Cmd):
                 print("Root: FALSE")
         else:
             pass
+
+    def do_ping(self, ip):
+        """Pings an IP-address. Usage: ping 192.168.178.1"""
+        os.system("ping " + str(ip))  # A very lazy way to ping.
 
     # If you did a contest to see what the most unreadable piece of code was,
     # I think this might win! Don't worry though, I'll add comments (to ease the pain). (I'm coping holy hell.)
@@ -329,7 +478,9 @@ class SSPCI(cmd.Cmd):
                         else:
                             print("No text specified for writing to file.")
                     else:
-                        print("No such file.")
+                        print("Insufficient permissions to access file.")
+                else:
+                    print("No such file.")
             elif splitOption[0] == "--create" or splitOption[0] == "-c":
                 filename = str(splitOption[1])
                 if filename:
@@ -370,7 +521,13 @@ class SSPCI(cmd.Cmd):
                 else:
                     print("No such file.")
             else:
-                print("Incorrect parameter.")
+                print(splitOption[0] +  ": incorrect parameter")
+                print("Usage: file -c amazingFile.txt")
+                print("Parameters are:\n-c or --create: creates a file\n-w or --write: Writes to a file")
+                print("-W or --overwrite: Overwrites a file\n-r or --read: Reads from a file")
+                print("-cp or --copy: Copies a file")
+                print("For -cp or --copy usage is different. You must select a source file and destination file.")
+                print("To do this, usage is: file --copy amazingFile.txt /home/example/exampleFolder/exampleDocument.txt")
         else:
             print("Usage: file -c amazingFile.txt")
             print("Parameters are:\n-c or --create: creates a file\n-w or --write: Writes to a file")
@@ -378,6 +535,8 @@ class SSPCI(cmd.Cmd):
             print("-cp or --copy: Copies a file")
             print("For -cp or --copy usage is different. You must select a source file and destination file.")
             print("To do this, usage is: file --copy amazingFile.txt /home/example/exampleFolder/exampleDocument.txt")
+
+
 
     # TODO: Maybe update this garbage?!? Meh, don't feel like it.
     def do_write(self, filename):  # The same as file -w, but for reasons it's still here.
@@ -423,6 +582,16 @@ class SSPCI(cmd.Cmd):
         else:
             print("Usage: write amazingFile.txt")
 
+    def do_scl(self, param):
+        split_param = param.split(" ", -1)
+        profilename = split_param[1]
+
+
+    def do_xtnd(self, extension):
+        """Extension manager for SSPCI, not the same as synk, although xtnd will be implemented in synk later"""
+
+
+
     def do_overwrite(self, filename):
         """Overwrites a file. Usage: overwrite amazingFileName.txt
         WARNING!! THIS WILL OVERWRITE ANY FILE IT HAS PERMISSIONS TO!! MAKE SURE YOU'RE CHOOSING THE CORRECT FILE!!"""
@@ -463,7 +632,7 @@ class SSPCI(cmd.Cmd):
 
     def do_ver(self, none):  # It prints whatever version you're running.
         """Prints the current version of SSPCI."""
-        print("SSPCI Version 0.5.0-alpha")
+        print("SSPCI Version 0.6.0-unstable")
         print("Made by NovaCow")
 
     def do_exit(self, none):  # It ends, what more do you want?
